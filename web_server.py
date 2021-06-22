bd1c8bc09bda04dffff08c4871e3d9de9755305c
VelvetyDrop <fukkun0929@gmail.com>
2021-06-22 15:01:21 +0900

    Update web_server.py

--- web_server.py
+++ web_server.py
@@ -3,6 +3,10 @@
 from flask import Flask , render_template, request
 from sqlalchemy import *
 
+app.config["JSON_AS_ASCII"]  = False
+app.config["JSON_SORT_KEYS"] = False
+
+
 def db_connect(db_name):
     USER_NAME = ""
     PASSWORD  = ""
@@ -34,6 +38,8 @@ def display_page():
     question = engine.execute("質問取ってくるSQL文")
     answer = engine.execute("回答取ってくるSQL文")
     
+    
+    
     return render_template("index.html",hyoudai = hyoudai,question = question, answer = answer)
     
     
@@ -48,5 +54,5 @@ def post():
     return render_template("result.html", kaitou = kaitou)
     
 if __name__ == "__main__":
-    app.run(debug = True)
+    app.run()
     
